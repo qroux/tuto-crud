ec73e02c7c6a546ffb8cd860314248362c08e2f7
qroux <quentin.roux@hotmail.fr>
2021-04-07 19:07:43 +0200

    Authentication => Passport + authStrategy implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [ItemsModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
